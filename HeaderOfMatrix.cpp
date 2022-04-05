ab311d79905389cbf5afe1d3fb6fba92174838fd
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-04-05 07:25:21 +0300

    Add files via upload

--- HeaderOfMatrix.cpp
+++ HeaderOfMatrix.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <ctime>
 
-void printOfMatrix(double** matrix, int* line_A, int* column_B)
+void printOfMatrix(double** matrix, int line_A, int column_B)
 {
-	for (int i = 0; i < *line_A; i++)// вывожу итоговую матрицу
+	for (int i = 0; i < line_A; i++)// вывожу итоговую матрицу
 	{
-		for (int j = 0; j < *column_B; j++)
+		for (int j = 0; j < column_B; j++)
 		{
 			std::cout << matrix[i][j];
 			std::cout << " ";
@@ -14,14 +14,14 @@ void printOfMatrix(double** matrix, int* line_A, int* column_B)
 	}
 }
 
-void multiplicationOfMatrix(double** matrix1, double** matrix2, double** matrix3, int* line_first_matrix, int* column_first_matrix, int* column_second_matrix)
+void multiplicationOfMatrix(double** matrix1, double** matrix2, double** matrix3, int line_first_matrix, int column_first_matrix, int column_second_matrix)
 {
-	for (int i = 0; i < *line_first_matrix; i++) // вычисл€ю элементы
+	for (int i = 0; i < line_first_matrix; i++) // вычисл€ю элементы
 	{
-		for (int j = 0; j < *column_second_matrix; j++)
+		for (int j = 0; j < column_second_matrix; j++)
 		{
 			matrix3[i][j] = 0;
-			for (int k = 0; k < *column_first_matrix; k++)
+			for (int k = 0; k < column_first_matrix; k++)
 			{
 				matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
 			}
@@ -29,14 +29,14 @@ void multiplicationOfMatrix(double** matrix1, double** matrix2, double** matrix3
 	}
 }
 
-void fillMatrixRand(double** matrix, int* line, int* column)
+void fillMatrixRand(double** matrix, int line, int column)
 {
-	for (int i = 0; i < *line; i++)
+	for (int i = 0; i < line; i++)
 	{
-		for (int j = 0; j < *column; j++)
+		for (int j = 0; j < column; j++)
 		{
 			matrix[i][j] = rand();
-			if (*line * (*column) < 1000)
+			if (line * (column) < 1000)
 			{
 				std::cout << "[" << i << "][" << j << "]= ";
 				std::cout << matrix[i][j] << std::endl;
@@ -45,20 +45,20 @@ void fillMatrixRand(double** matrix, int* line, int* column)
 	}
 }
 
-void deleteOfMatrix(double** matrix, int* line, int* column)
+void deleteOfMatrix(double** matrix, int line, int column)
 {
-	for (int count = 0; count < *line; count++)
+	for (int count = 0; count < line; count++)
 	{
 		delete[] matrix[count];
 	}
 	delete[] matrix;
 }
 
-void fillMatrix(double** matrix, int* line, int* column)
+void fillMatrix(double** matrix, int line, int column)
 {
-	for (int i = 0; i < *line; i++)
+	for (int i = 0; i < line; i++)
 	{
-		for (int j = 0; j < *column; j++)
+		for (int j = 0; j < column; j++)
 		{
 			std::cout << "[" << i << "][" << j << "]= ";
 			std::cin >> matrix[i][j];
@@ -66,19 +66,19 @@ void fillMatrix(double** matrix, int* line, int* column)
 	}
 }
 
-void malloxMatrix(double**& matrix, int* line, int* column)
+void malloxMatrix(double**& matrix, int line, int column)
 {
-	matrix = new double* [*line];
-	for (int i = 0; i < *line; i++)//¬вожу элементы первой матрицы
+	matrix = new double* [line];
+	for (int i = 0; i < line; i++)//¬вожу элементы первой матрицы
 	{
-		matrix[i] = new double[*column];
+		matrix[i] = new double[column];
 	}
 }
 
-void proverka(int* x)
+void proverka(int x)
 {
 	std::cin.clear();
 	std::cin.ignore(32767, '\n');
 	std::cout << "¬ведите значение повторно:";
-	std::cin >> *x;
+	std::cin >> x;
 }
--- Матрицы 3.cpp
+++ Матрицы 3.cpp
@@ -7,17 +7,17 @@ void fill_lines_and_columns(int* nRef)
 	std::cin >> *nRef;
 	while (std::cin.fail())
 	{
-		proverka(*&nRef);
+		proverka(*nRef);
 	}
 }
 
-void fillOrRand(int* questions)
+void fillOrRand(int *questions)
 {
 	std::cout << "Вы хотите ввести элементы массива вручную или попробовать ввод рандомных числа?\nВручную - 1\nВвод рандомных чисел - 2" << std::endl;
 	std::cin >> *questions;
 	while (*questions != 1 && *questions != 2)
 	{
-		proverka(*&questions);
+		proverka(*questions);
 	}
 }
 
@@ -44,44 +44,44 @@ int main()
 		fill_lines_and_columns(&column_B);
 	} while (column_A != line_B);
 
-	malloxMatrix(a, &line_A, &column_A);
+	malloxMatrix(a, line_A, column_A);
 	fillOrRand(&questions);
 	if (questions == 1)
 	{
 		std::cout << "Введите элементы матрицы" << std::endl;
-		fillMatrix(a, &line_A, &column_A);
+		fillMatrix(a, line_A, column_A);
 	}
 	else if (questions == 2)
 	{
 		std::cout << "Вводятся рандомные значения элементов матрицы" << std::endl;
-		fillMatrixRand(a, &line_A, &column_A);
+		fillMatrixRand(a, line_A, column_A);
 	}
 
-	malloxMatrix(b, &line_B, &column_B);
+	malloxMatrix(b, line_B, column_B);
 	fillOrRand(&questions);
 	if (questions == 1)
 	{
 		std::cout << "Введите элементы матрицы" << std::endl;
-		fillMatrix(b, &line_B, &column_B);
+		fillMatrix(b, line_B, column_B);
 	}
 	else if (questions == 2)
 	{
 		std::cout << "Вводятся рандомные значения элементов матрицы" << std::endl;
-		fillMatrixRand(b, &line_B, &column_B);
+		fillMatrixRand(b, line_B, column_B);
 	}
 
-	malloxMatrix(c, &line_A, &column_B);
+	malloxMatrix(c, line_A, column_B);
 	double clock_1 = clock();
-	multiplicationOfMatrix(a, b, c, &line_A, &column_A, &column_B);
+	multiplicationOfMatrix(a, b, c, line_A, column_A, column_B);
 	double clock_2 = clock();
 	double overall_time = clock_2 - clock_1;
 	std::cout << "Общее время выполенения умножения двух матриц = " << overall_time/1000 << " секунд" << std::endl;
 	if (line_A * column_B < 1000)
 	{
-		printOfMatrix(c, &line_A, &column_B);
+		printOfMatrix(c, line_A, column_B);
 	}
-	deleteOfMatrix(a, &line_A, &column_A);
-	deleteOfMatrix(b, &line_B, &column_B);
-	deleteOfMatrix(c, &line_A, &column_B);
+	deleteOfMatrix(a, line_A, column_A);
+	deleteOfMatrix(b, line_B, column_B);
+	deleteOfMatrix(c, line_A, column_B);
 	return 0;
 }
